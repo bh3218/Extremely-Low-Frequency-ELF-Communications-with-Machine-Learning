9c3bc0e7cf459367740caf3574ec5869d13db53e
bh3218 <94368896+bh3218@users.noreply.github.com>
2022-06-20 17:35:16 +0100

    Create Write_Excel.m

---
+++ MATLAB Code/Write_Excel.m
@@ -0,0 +1,15 @@
+% MATLAB script to write a specified matrix or array to an excel file or
+% text file for storage of data
+
+% Clear all variables and close all figures
+clear;
+close all;
+
+writematrix(array, 'Filename.xlsx')
+
+
+
+
+
+
+
