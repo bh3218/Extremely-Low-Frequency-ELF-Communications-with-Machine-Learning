634c59f291fe7d4d2285e270a099a5454c7f3bf2
bh3218 <94368896+bh3218@users.noreply.github.com>
2022-06-28 10:31:08 +0100

    Create load_model.py

---
+++ Python Code/load_model.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+
+model_conv1d = keras.models.load_model('Trained Model/Conv1DLSTM_BEST_RandomLowdBTwoAmp_11062022.h5')
